0180e25f7b376171f20cfea51c754c0e1a58c3a4
Linda Lam <lindaqlam@gmail.com>
2020-06-23 20:53:33 -0700

    add albums, move songs to separate file

---
+++ songs.js
@@ -0,0 +1,515 @@
+var allSongs = [
+	{
+		album: 'Lover',
+		song: 'I Forgot That You Existed',
+		lyric: 'Taught me some hard lessons, I just forget what they were'
+	},
+	{
+		album: 'Lover',
+		song: 'Cruel Summer',
+		lyric: "I'm always waiting for you to be waiting below"
+	},
+	{
+		album: 'Lover',
+		song: 'Lover',
+		lyric: "I'm highly suspicious that everyone who sees you wants you"
+	},
+	{
+		album: 'Lover',
+		song: 'Cruel Summer',
+		lyric: 'He looks up, grinning like a devil'
+	},
+	{
+		album: 'Lover',
+		song: 'The Man',
+		lyric: "I'd be just like Leo, in Saint-Tropez"
+	},
+	{
+		album: 'Lover',
+		song: 'The Archer',
+		lyric: "I've got a hundred thrown-out speeches I almost said to you"
+	},
+	{
+		album: 'Lover',
+		song: 'I Think He Knows',
+		lyric: "Wanna see what's under that attitude"
+	},
+	{
+		album: 'Lover',
+		song: 'I Think He Knows',
+		lyric: "We can follow the sparks, I'll drive"
+	},
+	{
+		album: 'Lover',
+		song: 'Miss Americana and the Heartbreak Prince',
+		lyric: 'The damsels are depressed'
+	},
+	{
+		album: 'Lover',
+		song: 'Miss Americana and the Heartbreak Prince',
+		lyric: "We're so sad, we paint the town blue"
+	},
+	{
+		album: 'Lover',
+		song: 'Paper Rings',
+		lyric: "Now I've read all of the books beside your bed"
+	},
+	{
+		album: 'Lover',
+		song: 'Paper Rings',
+		lyric: "I'm with you even if it makes me blue"
+	},
+	{
+		album: 'Lover',
+		song: 'Cornelia Street',
+		lyric: 'And baby, I get mystified by how this city screams your name'
+	},
+	{
+		album: 'Lover',
+		song: 'Death By A Thousand Cuts',
+		lyric: 'I look through the windows of this love, even though we boarded them up'
+	},
+	{
+		album: 'Lover',
+		song: 'London Boy',
+		lyric: "Show me a gray sky, a rainy cab ride. Babes, don't threaten me with a good time"
+	},
+	{
+		album: 'Lover',
+		song: "Soon You'll Get Better",
+		lyric: 'Desperate people find faith, so now I pray to Jesus too'
+	},
+	{
+		album: 'Lover',
+		song: "Soon You'll Get Better",
+		lyric: 'I know delusion when I see it in the mirror'
+	},
+	{
+		album: 'Lover',
+		song: 'False God',
+		lyric: 'Daring you to leave me just so I can try and scare you'
+	},
+	{
+		album: 'Lover',
+		song: 'You Need To Calm Down',
+		lyric: 'We see you over there on the internet comparing all the girls who are killing it'
+	},
+	{
+		album: 'Lover',
+		song: 'Afterglow',
+		lyric: 'This ultraviolet morning light below tells me this love is worth the fight, oh'
+	},
+	{
+		album: 'Lover',
+		song: 'ME!',
+		lyric: "I'm the only one of me. Baby, that's the fun of me"
+	},
+	{
+		album: 'Lover',
+		song: "It's Nice To Have A Friend",
+		lyric: 'Something gave you the nerve to touch my hand'
+	},
+	{
+		album: 'Lover',
+		song: 'Daylight',
+		lyric: "I wounded the good and I trusted the wicked. Clearin' the air, I breathed in the smoke"
+	},
+	{
+		album: 'Lover',
+		song: 'Daylight',
+		lyric: "I once believed love would be black and white but it's golden"
+	},
+
+	{
+		album: 'Reputation',
+		song: '...Ready for It?',
+		lyric: 'Me, I was a robber first time that he saw me. Stealing hearts and running off and never saying sorry'
+	},
+	{
+		album: 'Reputation',
+		song: 'Endgame',
+		lyric: "I bury hatchets but I keep maps of where I put 'em"
+	},
+	{
+		album: 'Reputation',
+		song: 'I Did Something Bad',
+		lyric: "If he drops my name, then I owe him nothin'"
+	},
+	{
+		album: 'Reputation',
+		song: 'I Did Something Bad',
+		lyric: "They're burning all the witches, even if you aren't one"
+	},
+	{
+		album: 'Reputation',
+		song: "Don't Blame Me",
+		lyric: 'My name is whatever you decide'
+	},
+	{
+		album: 'Reputation',
+		song: "Don't Blame Me",
+		lyric: "I once was poison ivy, but now I'm your daisy"
+	},
+	{
+		album: 'Reputation',
+		song: 'Delicate',
+		lyric: "My reputation's never been worse, so you must like me for me"
+	},
+	{
+		album: 'Reputation',
+		song: 'Delicate',
+		lyric: 'Phone lights up my nightstand in the black. Come here, you can meet me in the back'
+	},
+	{
+		album: 'Reputation',
+		song: 'Look What You Made Me Do',
+		lyric: "I've got a list of names and yours is in red, underlined"
+	},
+	{
+		album: 'Reputation',
+		song: 'Look What You Made Me Do',
+		lyric: "I don't like your kingdom keys. They once belonged to me"
+	},
+	{
+		album: 'Reputation',
+		song: 'So It Goes...',
+		lyric: "You did a number on me. But, honestly, baby, who's counting?"
+	},
+	{
+		album: 'Reputation',
+		song: 'So It Goes..',
+		lyric: "I'm yours to keep, and I'm yours to lose"
+	},
+	{
+		album: 'Reputation',
+		song: 'Gorgeous',
+		lyric: 'You should think about the consequence of your magnetic field being a little too strong'
+	},
+	{
+		album: 'Reputation',
+		song: 'Gorgeous',
+		lyric: "You should take it as a compliment that I'm talking to everyone here but you"
+	},
+	{
+		album: 'Reputation',
+		song: 'Gorgeous',
+		lyric: "If you've got a girlfriend, I'm jealous of her. But if you're single that's honestly worse"
+	},
+	{
+		album: 'Reputation',
+		song: 'Getaway Car',
+		lyric: 'He was the best of times, the worst of crimes'
+	},
+	{
+		album: 'Reputation',
+		song: 'Getaway Car',
+		lyric: 'The ties were black, the lies were white'
+	},
+	{
+		album: 'Reputation',
+		song: 'Getaway Car',
+		lyric: 'I knew it from the first Old Fashioned, we were cursed'
+	},
+	{
+		album: 'Reputation',
+		song: 'Getaway Car',
+		lyric: 'Put the money in a bag and I stole the keys. That was the last time you ever saw me'
+	},
+	{
+		album: 'Reputation',
+		song: 'King of My Heart',
+		lyric: "Now you try on callin' me, baby, like tryin' on clothes"
+	},
+	{
+		album: 'Reputation',
+		song: 'King of My Heart',
+		lyric: "Your love is a secret I'm hoping, dreaming, dying to keep"
+	},
+	{
+		album: 'Reputation',
+		song: 'Dancing With Our Hands Tied',
+		lyric: 'Deep blue, but you painted me golden'
+	},
+	{
+		album: 'Reputation',
+		song: 'Dancing With Our Hands Tied',
+		lyric: "I'm a mess, but I'm the mess that you wanted"
+	},
+	{
+		album: 'Reputation',
+		song: 'Dancing With Our Hands Tied',
+		lyric: "I'd kiss you as the lights went out, swaying as the room burned down"
+	},
+	{
+		album: 'Reputation',
+		song: 'Dress',
+		lyric: 'There is an indentation in the shape of you'
+	},
+	{
+		album: 'Reputation',
+		song: 'Dress',
+		lyric: "I don't want you like a best friend"
+	},
+	{
+		album: 'Reputation',
+		song: 'Dress',
+		lyric: 'Your buzzcut and my hair bleached. Even in my worst times, you could see the best of me'
+	},
+	{
+		album: 'Reputation',
+		song: "This Is Why We Can't Have Nice Things",
+		lyric: 'Feeling so Gatsby for that whole year'
+	},
+	{
+		album: 'Reputation',
+		song: "This Is Why We Can't Have Nice Things",
+		lyric: "Did you think I wouldn't hear all the things you said about me?"
+	},
+	{
+		album: 'Reputation',
+		song: "This Is Why We Can't Have Nice Things",
+		lyric: "Here's a toast to my real friends. They don't care about that he said, she said"
+	},
+	{
+		album: 'Reputation',
+		song: "This Is Why We Can't Have Nice Things",
+		lyric: "Here's to my momma. Had to listen to all this drama"
+	},
+	{
+		album: 'Reputation',
+		song: 'Call It What You Want',
+		lyric: "I brought a knife to a gunfight. They took the crown, but it's alright"
+	},
+	{
+		album: 'Reputation',
+		song: 'Call It What You Want',
+		lyric: "My baby's fly like a jet stream, high above the whole scene, loves me like I'm brand new"
+	},
+	{
+		album: 'Reputation',
+		song: 'Call It What You Want',
+		lyric: 'All my flowers grew back as thorns, windows boarded up after the storm'
+	},
+	{
+		album: 'Reputation',
+		song: 'Call It What You Want',
+		lyric: "Holdin' my breath, slowly I said, 'You don't need to save me, but would you run away with me?'"
+	},
+	{
+		album: 'Reputation',
+		song: "New Year's Day",
+		lyric: "You squeeze my hand three times in the back of the taxi, I can tell that it's going to be a long road"
+	},
+	{
+		album: 'Reputation',
+		song: "New Year's Day",
+		lyric: "Please don't ever become a stranger whose laugh I could recognize anywhere"
+	},
+
+	{
+		album: '1989',
+		song: 'Welcome To New York',
+		lyric: 'Everybody here was someone else before'
+	},
+	{
+		album: '1989',
+		song: 'Welcome To New York',
+		lyric: 'Kaleidoscope of loud heartbeats under coats'
+	},
+	{
+		album: '1989',
+		song: 'Blank Space',
+		lyric: "Love's a game, want to play?"
+	},
+	{
+		album: '1989',
+		song: 'Blank Space',
+		lyric: 'I can read you like a magazine'
+	},
+	{
+		album: '1989',
+		song: 'Blank Space',
+		lyric: "'Cause you know I love the players, and you love the game"
+	},
+	{
+		album: '1989',
+		song: 'Blank Space',
+		lyric: "'Cause darling I'm a nightmare dressed like a daydream"
+	},
+	{
+		album: '1989',
+		song: 'Blank Space',
+		lyric: "Boys only want love if it's torture"
+	},
+	{
+		album: '1989',
+		song: 'Style',
+		lyric: "So it goes, he can't keep his wild eyes on the road"
+	},
+	{
+		album: '1989',
+		song: 'Style',
+		lyric: 'When we go crashing down, we come back every time'
+	},
+	{
+		album: '1989',
+		song: 'Out of the Woods',
+		lyric:
+			'You took a Polaroid of us then discovered the rest of the world was black and white, but we were in screaming color'
+	},
+	{
+		album: '1989',
+		song: 'Out of the Woods',
+		lyric: 'We were built to fall apart, then fall back together'
+	},
+	{
+		album: '1989',
+		song: 'All You Had To Do Was Stay',
+		lyric: 'People like you always want back the love they gave away'
+	},
+	{
+		album: '1989',
+		song: 'Shake It Off',
+		lyric: "I go on too many dates, but I can't make 'em stay. At least that's what people say"
+	},
+	{
+		album: '1989',
+		song: 'I Wish You Would',
+		lyric: 'You gave me everything and nothing'
+	},
+	{
+		album: '1989',
+		song: 'I Wish You Would',
+		lyric: 'Wish you knew that I miss you too much to be mad anymore'
+	},
+	{
+		album: '1989',
+		song: 'I Wish You Would',
+		lyric: "You think I'm gonna hate you now 'cause you still don't know what I never said"
+	},
+	{
+		album: '1989',
+		song: 'Bad Blood',
+		lyric: 'If you love like that blood runs cold'
+	},
+	{
+		album: '1989',
+		song: 'Wildest Dreams',
+		lyric: 'Some day when you leave me, I bet these memories follow you around'
+	},
+	{
+		album: '1989',
+		song: 'Wildest Dreams',
+		lyric: 'I can see the end as it begins'
+	},
+	{
+		album: '1989',
+		song: 'How You Get The Girl',
+		lyric: "Broke your heart, I'll put it back together"
+	},
+	{
+		album: '1989',
+		song: 'This Love',
+		lyric: 'Tossing, turning, struggle through the night for someone new'
+	},
+	{
+		album: '1989',
+		song: 'This Love',
+		lyric: 'These hands had to let it go free and this love came back to me'
+	},
+	{
+		album: '1989',
+		song: 'This Love',
+		lyric: "When you're young, you just run. But you come back to what you need"
+	},
+	{
+		album: '1989',
+		song: 'I Know Places',
+		lyric: "Love's a fragile little flame, it could burn out"
+	},
+	{
+		album: '1989',
+		song: 'I Know Places',
+		lyric: 'Loose lips sink ships all the damn time, not this time'
+	},
+	{
+		album: '1989',
+		song: 'I Know Places',
+		lyric: 'They are the hunters, we are the foxes. And we run'
+	},
+	{
+		album: '1989',
+		song: 'Clean',
+		lyric: "The flowers that we'd grown together died of thirst"
+	},
+	{
+		album: '1989',
+		song: 'Clean',
+		lyric: 'When the butterflies turned to dust, they covered my whole room'
+	},
+	{
+		album: '1989',
+		song: 'Clean',
+		lyric: 'The water filled my lungs, I screamed so loud but no one heard a thing'
+	},
+	{
+		album: '1989',
+		song: 'Wonderland',
+		lyric: "Haven't you heard what becomes of curious minds"
+	},
+	{
+		album: '1989',
+		song: 'Wonderland',
+		lyric: "I should've slept with one eye opened at night"
+	},
+	{
+		album: '1989',
+		song: 'Wonderland',
+		lyric: 'But there were strangers watching, and whispers turned to talking, and talking turned to screams'
+	},
+	{
+		album: '1989',
+		song: 'You Are in Love',
+		lyric: 'No proof, not much, but you saw enough'
+	},
+	{
+		album: '1989',
+		song: 'You Are in Love',
+		lyric: 'You keep his shirt, he keeps his word'
+	},
+	{
+		album: '1989',
+		song: 'You Are in Love',
+		lyric: 'He keeps a picture of you in his office downtown'
+	},
+	{
+		album: '1989',
+		song: 'New Romantics',
+		lyric: 'We show off our different scarlet letters. Trust me, mine is better'
+	},
+	{
+		album: '1989',
+		song: 'New Romantics',
+		lyric: "We play dumb but we know exactly what we're doing"
+	},
+	{
+		album: '1989',
+		song: 'New Romantics',
+		lyric: 'Honey, life is just a classroom'
+	},
+	{
+		album: '1989',
+		song: 'New Romantics',
+		lyric: 'I could build a castle out of all the bricks they threw at me'
+	},
+	{
+		album: '1989',
+		song: 'New Romantics',
+		lyric: 'The best people in life are free'
+	},
+	{
+		album: '1989',
+		song: 'New Romantics',
+		lyric: 'The rumors are terrible and cruel but honey most of them are true'
+	}
+];
